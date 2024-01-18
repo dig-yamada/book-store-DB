13e5870d5246bfa9d82ad53f3cf180bd5ed53049
dig-yamada <t-yamada@digidelic.jp>
2024-01-18 17:45:27 +0900

    .gitignore added

---
+++ config.js
@@ -0,0 +1,5 @@
+export const PORT = process.env.PORT || 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://root:UisLuFS9XpPNMMLT@books-store-mern.igqcyen.mongodb.net/books-collection?retryWrites=true&w=majority'
+   //mongodb+srv://root:UisLuFS9XpPNMMLT@books-store-mern.igqcyen.mongodb.net/?retryWrites=true&w=majority
